3da1d81946aee35d66d8b96f32ac346c710bfc02
Hussein-Hossam-Idris <36144142+Hussein-Hossam-Idris@users.noreply.github.com>
2021-04-30 01:35:45 +0200

    added more home page functionalities
    added home page search bar,
    added home page categories place holder,
    added navigators to drawer

--- auto_buy/lib/screens/home_page/home_page_screen.dart
+++ auto_buy/lib/screens/home_page/home_page_screen.dart
@@ -1,6 +1,13 @@
+import 'package:auto_buy/screens/expense_tracker/expense_tracker_screen.dart';
+import 'package:auto_buy/screens/friends/friends_screen.dart';
+import 'package:auto_buy/screens/monthly_supplies/monthly_supplies_screen.dart';
+import 'package:auto_buy/screens/my_orders/my_orders_screen.dart';
+import 'package:auto_buy/screens/shopping_cart/shopping_cart_screen.dart';
+import 'package:auto_buy/screens/user_account/user_account_screen.dart';
+import 'package:auto_buy/screens/wishlist/wishlist_screen.dart';
 import 'package:auto_buy/services/firebase_auth_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_search_bar/flutter_search_bar.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,22 +16,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  SearchBar searchBar;
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<FirebaseAuthService>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        actions: [FlatButton(onPressed: auth.signOut, child: Text("Logout"))],
+        iconTheme: IconThemeData(color: Colors.white),
+        flexibleSpace: customSearchBar(),
+        actions: [
+        ],
       ),
-      drawer: _drawer(),
-
+      drawer: _drawer(context),
+      body: Container(
+        height: 100,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 30,
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+              height: 30,
+            ),
+            Container(
+              color: Colors.green,
+              width: 100,
+              height: 30,
+            ),
+            Container(
+              color: Colors.black,
+              width: 100,
+              height: 30,
+            ),
+            Container(
+              color: Colors.lightGreen,
+              width: 100,
+              height: 30,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-Widget _drawer(){
+Widget _drawer(BuildContext context) {
+  final auth = Provider.of<FirebaseAuthService>(context, listen: false);
   return Drawer(
     child: ListView(
       // Important: Remove any padding from the ListView.
@@ -33,88 +73,174 @@ Widget _drawer(){
         Container(
           padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
           child: Center(
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.menu_outlined,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.menu_outlined,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Main Menu',
+                  style: TextStyle(
                     color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Main Menu',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
+            ),
           ),
           decoration: BoxDecoration(
             color: Colors.orange,
           ),
         ),
-        customTextStle('Expense Tracker',
+        customTextStle(
+            'Expense Tracker',
             Icon(
               Icons.analytics_sharp,
               color: Colors.green,
               size: 30,
             ),
-            (){}
-        ),
-        customTextStle('Monthly Supplies',
+            () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => ExpenseTrackerScreen(),
+                ),
+              );
+            }),
+        customTextStle(
+            'Monthly Supplies',
             Icon(
-              Icons.shopping_cart,
+              Icons.calendar_today_outlined,
               color: Colors.orange,
             ),
-                (){}
-        ),
-        customTextStle('My WishList',
+            () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => MonthlySuppliesScreen(),
+                ),
+              );
+            }),
+        customTextStle(
+            'My WishList',
             Icon(
               Icons.favorite,
               color: Colors.red,
             ),
-                (){}
-        ),
-        customTextStle('Shopping Cart',
+            () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => WishListScreen(),
+                ),
+              );
+            }),
+        customTextStle(
+            'Shopping Cart',
             Icon(
               Icons.add_shopping_cart_outlined,
               color: Colors.blue,
             ),
-                (){}
-        ),
-        customTextStle('User Account',
+            () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => ShoppingCartScreen(),
+                ),
+              );
+            }),
+        customTextStle(
+            'User Account',
             Icon(
               Icons.person,
               color: Colors.black,
             ),
-                (){}
-        ),
-        customTextStle('Friends',
+            () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => UserAccountScreen(),
+                ),
+              );
+            }),
+        customTextStle(
+            'Friends',
             Icon(
               Icons.people,
               color: Colors.black,
             ),
-                (){}
-        ),
-        customTextStle('My Orders',
+            () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => FriendsScreen(),
+                ),
+              );
+            }),
+        customTextStle(
+            'My Orders',
             Icon(
               Icons.list_alt,
-              color: Colors.amber,
+              color: Colors.purpleAccent,
             ),
-                (){}
-        ),
+            () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => MyOrdersScreen(),
+                ),
+              );
+            }),
+        customTextStle(
+            'LogOut',
+            Icon(
+              Icons.logout,
+              color: Colors.redAccent,
+            ),
+                () {
+                auth.signOut();
+                }),
       ],
     ),
   );
 }
 
-Widget customTextStle(String text,Widget icon,VoidCallback onTap){
+Widget customTextStle(String text, Widget icon, VoidCallback onTap) {
   return ListTile(
     title: Text(text),
-    leading:icon,
+    leading: icon,
     onTap: onTap,
+  );
+}
+
+Widget customSearchBar() {
+  return Padding(
+    padding: EdgeInsets.fromLTRB(50, 5, 20, 5),
+    child: TextFormField(
+      decoration: InputDecoration(
+        border: UnderlineInputBorder(
+          borderRadius: const BorderRadius.all(
+            const Radius.circular(25.0),
+          ),
+        ),
+        hintText: 'type something',
+        fillColor: Colors.white,
+        filled: true,
+        //TODO: search button function
+        suffixIcon: IconButton(icon: Icon(Icons.tag_faces), onPressed: () {}),
+      ),
+    ),
   );
 }
