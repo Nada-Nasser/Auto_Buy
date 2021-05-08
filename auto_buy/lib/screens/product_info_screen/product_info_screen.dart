a597ba1238392ad60b2ec29bfdd539a40dca3696
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2021-05-08 22:46:02 +0200

    design product_info_screen.dart

--- auto_buy/lib/screens/product_info_screen/product_info_screen.dart
+++ auto_buy/lib/screens/product_info_screen/product_info_screen.dart
@@ -18,7 +18,7 @@ class _ProductInfoScreenState extends State<ProductInfoScreen> {
 
   double get price => quantity * widget.product.price;
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  bool isProductInWishList = true;
+  bool isProductInWishList = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,31 +44,39 @@ class _ProductInfoScreenState extends State<ProductInfoScreen> {
       _buildProductNameAndPriceWidget(context),
       _buildProductImageWidget(context),
       _buildQuantityAndTotalPriceWidget(context),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            onPressed: () {
-              setState(() {
-                isProductInWishList = !isProductInWishList;
-                showInSnackBar("Product added to your wish list");
-              });
-            },
-            icon: Icon(
-              isProductInWishList ? Icons.favorite : Icons.favorite_border,
-              color: isProductInWishList ? Colors.red : Colors.black,
-            ),
+      _addingToCartsWidgets(context),
+      Container(
+        margin: EdgeInsets.fromLTRB(5, 30, 5, 20),
+        width: MediaQuery.of(context).size.width,
+        decoration: _boxDecoration(Colors.black),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "Product Description",
+                  style: TextStyle(
+                    fontSize: 0.05 * MediaQuery.of(context).size.width,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  widget.product.description,
+                  textAlign: TextAlign.start,
+                  softWrap: true,
+                ),
+              ),
+            ],
           ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart_outlined,
-            ),
-            label: Text(
-              "Add to Shopping Cart",
-            ),
-          ),
-        ],
+        ),
       ),
     ];
 
@@ -79,6 +87,63 @@ class _ProductInfoScreenState extends State<ProductInfoScreen> {
         itemBuilder: (BuildContext context, int index) {
           return content[index];
         },
+      ),
+    );
+  }
+
+  Widget _addingToCartsWidgets(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(4, 30, 4, 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _addingToWishListWidget(context),
+          _addingToShoppingCartWidget(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _addingToShoppingCartWidget(BuildContext context) {
+    return Container(
+      height: 0.15 * MediaQuery.of(context).size.width,
+      decoration: _boxDecoration(Colors.black),
+      child: OutlinedButton.icon(
+        onPressed: () {},
+        icon: Icon(
+          Icons.shopping_cart_outlined,
+          size: 0.1 * MediaQuery.of(context).size.width,
+        ),
+        label: Text(
+          "Add to Shopping Cart",
+          softWrap: true,
+          style: TextStyle(
+            color: Colors.grey[800],
+            fontSize: 0.045 * MediaQuery.of(context).size.width,
+            //fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _addingToWishListWidget(BuildContext context) {
+    return Container(
+      height: 0.15 * MediaQuery.of(context).size.width,
+      width: 0.15 * MediaQuery.of(context).size.width,
+      decoration: _boxDecoration(Colors.black),
+      child: IconButton(
+        iconSize: 0.1 * MediaQuery.of(context).size.width,
+        onPressed: () {
+          setState(() {
+            isProductInWishList = !isProductInWishList;
+            showInSnackBar("Product added to your wish list");
+          });
+        },
+        icon: Icon(
+          isProductInWishList ? Icons.favorite : Icons.favorite_border,
+          color: isProductInWishList ? Colors.red : Colors.orange,
+        ),
       ),
     );
   }
@@ -110,31 +175,7 @@ class _ProductInfoScreenState extends State<ProductInfoScreen> {
     return Container(
       width: 0.40 * MediaQuery.of(context).size.width,
       padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black,
-          width: 2,
-        ), //Border.all
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            offset: const Offset(
-              2.0,
-              2.0,
-            ), //Offset
-            blurRadius: 5.0,
-            spreadRadius: 2.0,
-          ), //BoxShadow
-          BoxShadow(
-            color: Colors.white,
-            offset: const Offset(0.0, 0.0),
-            blurRadius: 0.0,
-            spreadRadius: 0.0,
-          ), //BoxShadow
-        ],
-      ), //BoxDecoration
-
+      decoration: _boxDecoration(Colors.black),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -213,7 +254,10 @@ class _ProductInfoScreenState extends State<ProductInfoScreen> {
           child: Text(
             widget.product.name,
             softWrap: true,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 0.06 * MediaQuery.of(context).size.width,
+            ),
             textAlign: TextAlign.start,
           ),
         ),
@@ -222,10 +266,37 @@ class _ProductInfoScreenState extends State<ProductInfoScreen> {
             'EGP ${widget.product.price.toStringAsFixed(2)}',
             style: TextStyle(
               fontWeight: FontWeight.bold,
-              fontSize: 20,
+              fontSize: 0.06 * MediaQuery.of(context).size.width,
             ),
           ),
         )
+      ],
+    );
+  }
+
+  BoxDecoration _boxDecoration(Color color) {
+    return BoxDecoration(
+      border: Border.all(
+        color: color,
+        width: 1.55,
+      ), //Border.all
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+        /*BoxShadow(
+          color: color,
+          offset: const Offset(
+            2.0,
+            2.0,
+          ), //Offset
+          blurRadius: 3.0,
+          spreadRadius: 1.0,
+        ), */
+        BoxShadow(
+          color: Colors.white,
+          offset: const Offset(0.0, 0.0),
+          blurRadius: 0.0,
+          spreadRadius: 0.0,
+        ), //BoxShadow
       ],
     );
   }
