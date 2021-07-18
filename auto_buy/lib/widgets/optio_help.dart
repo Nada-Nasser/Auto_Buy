6818857f2830f95ef92bec1fbcc84aceb43b85e0
Hussein-Hossam-Idris <36144142+Hussein-Hossam-Idris@users.noreply.github.com>
2021-07-18 20:11:05 +0200

    added the optio commands help in arabic

--- auto_buy/lib/widgets/optio_help.dart
+++ auto_buy/lib/widgets/optio_help.dart
@@ -9,37 +9,31 @@ Future<void> optioHelpWidget(BuildContext context) async {
       return SingleChildScrollView(
         child: AlertDialog(
           scrollable: true,
-          title: new Text("Optio commands guide", textAlign: TextAlign.start),
+          title: new Text("Optio commands guide", textAlign: TextAlign.center),
           content: Container(
             child: Column(
               children: [
-                optioHelpText('adding products to carts','add {quantity} {product-name} to [shopping cart/monthly cart]'),
-                // optioHelpText('اضافة مشتريات لعربة التسوق','قم باضافة الكمية اسم المنتج الى عربة التسوق'),
+                optioHelpText('لاضافة مشتريات لعربة التسوق','قم باضافة <الكمية> <اسم المنتج> الى عربة التسوق'),
                 SizedBox(
                   height: 15,
                 ),
-                optioHelpText('deleting products from carts', 'delete {product-name} from [shopping cart/monthly cart]'),
-                // optioHelpText('مسح المشتريات من عربة التسوق', 'قم بمسح اسم المنتج من عربة التسوق'),
+                optioHelpText('لمسح المشتريات من عربة التسوق', 'قم بمسح <اسم المنتج> من عربة التسوق'),
                 SizedBox(
                   height: 15,
                 ),
-                optioHelpText('to open pages','open [friends lists/shopping cart/monthly cart/expense tracker]'),
-                // optioHelpText('لفتح اي صفحة في التطبيق', ' افتح اسم الصفحة'),
+                optioHelpText('لفتح اي صفحة في التطبيق', ' افتح اسم الصفحة'),
                 SizedBox(
                   height: 15,
                 ),
-                optioHelpText('to search for products','search for {product-name}'),
-                // optioHelpText('للبحث عن منتجات', 'ابحث عن اسم المنتج'),
+                optioHelpText('للبحث عن منتجات', 'ابحث عن اسم المنتج'),
                 SizedBox(
                   height: 15,
                 ),
-                optioHelpText('to add friends','add friend'),
-                // optioHelpText('لاضافة صديق', 'قم باضافة صديق'),
+                optioHelpText('لاضافة صديق', 'قم باضافة صديق'),
                 SizedBox(
                   height: 15,
                 ),
-                optioHelpText('to delete friends','delete friend'),
-                // optioHelpText('لمسح صديق', 'قم بمسح صديق'),
+                optioHelpText('لمسح صديق', 'قم بمسح صديق'),
               ],
             ),
           ),
@@ -52,23 +46,14 @@ Future<void> optioHelpWidget(BuildContext context) async {
 Widget optioHelpText(String head, String body) {
   return Container(
     width: double.maxFinite,
-    // padding: EdgeInsets.all(5),
-    // decoration: BoxDecoration(
-    //   borderRadius: BorderRadius.circular(0),
-    //   color: Colors.white,
-    //   boxShadow: [
-    //     BoxShadow(
-    //       color: Colors.black.withOpacity(0.5),
-    //       blurRadius: 3,
-    //       offset: Offset(0, 3),
-    //     )
-    //   ],
-    // ),
+    padding: EdgeInsets.all(5),
     child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Text(
           head,
+          textAlign: TextAlign.right,
+          locale: Locale('ar',''),
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.orange,
@@ -77,6 +62,8 @@ Widget optioHelpText(String head, String body) {
         ),
         Text(
           body,
+          locale: Locale('ar',''),
+          textAlign: TextAlign.right,
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 14,
@@ -88,28 +75,4 @@ Widget optioHelpText(String head, String body) {
 
 }
 
-Widget optioHeaderText(String text) {
-  return Container(
-    margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-    child: Text(
-      text,
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        color: Colors.orange,
-        fontSize: 20,
-      ),
-    ),
-  );
-}
 
-Widget optioBodyText(String text) {
-  return Container(
-    child: Text(
-      text,
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 14,
-      ),
-    ),
-  );
-}
