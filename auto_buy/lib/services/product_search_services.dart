8e5a96bdd3af6c85228315e2d3dd537f1579fb37
farah afifi <36455840+farah-afifi@users.noreply.github.com>
2021-07-25 21:59:42 +0200

    Update product_search_services.dart

--- auto_buy/lib/services/product_search_services.dart
+++ auto_buy/lib/services/product_search_services.dart
@@ -42,33 +42,33 @@ class ProductSearchServices {
     if(searchTerm == "")
       return [];
 
-    for(String s in _productNamesList){
-      pairs.add(Pair(s.similarityTo(searchTerm) , s));
+    for(String s in _productNamesList) {
+      pairs.add(Pair(s.similarityTo(searchTerm), s));
+    }
+    pairs.sort((a,b) => b.element1.compareTo(a.element1));
 
+    for(Pair<double,String> p in pairs ) {
+      if (p.element1 > .5)
+        similarStrings.add(p.element2);
+      else break;
+    }
+    pairs = [];
+    for(String s in _productNamesList){
       List<String> sentenceSplit = searchTerm.split(" ");
         for (String word in sentenceSplit) {
-          if(word != "" && s.contains(word)) {
-            similarStrings.add(s);
-          }
-          List<String> prodwords = s.split(" ");
-          for(String w in prodwords){
+          List<String> prodWords = s.split(" ");
+          for(String w in prodWords){
             print("SIMILARITY OF $w  and $word = ${w.similarityTo(word)}");
 
             if(w.similarityTo(word) > .7)
               similarStrings.add(s);
           }
+          if(word != "" && s.contains(word)) {
+            similarStrings.add(s);
+          }
         }
-
-    }
-    pairs.sort((a,b)=> a.element1 < b.element1? 1 : 0);
-
-    for(Pair<double,String> p in pairs ) {
-      if (p.element1 > .4)
-        similarStrings.add(p.element2);
-      else break;
     }
 
-    print("DOHA DUHA ${StringSimilarity.compareTwoStrings("doha","duha")}");
     return similarStrings.toList();
   }
 
